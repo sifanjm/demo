ff5f1a17a3b060e61ddfc16e8e20b3296a83acf0
vboxuser <vboxuser@linuxvm.myguest.virtualbox.org>
2024-02-27 19:16:13 +0530

    dd log

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+private static Logger logger = LogManager.getLogger(HomeController.class);
+
+    @GetMapping("/home")
+    public String home(){
+        logger.info("home api is invoked");
+        return "home page";
+    }
+
+}
